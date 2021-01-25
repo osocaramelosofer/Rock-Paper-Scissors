0bba6c0b91970406cd347af43c0ba21638f892a4
ferras <osocaramelosofer@hotmail.com>
2021-01-24 18:09:01 -0600

    cambios en modalRules

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-const bntRules = document.querySelector('.rulesModalButton');
+const bntRules   = document.querySelector('.rulesModalButton');
 const rulesModal = document.querySelector('.rulesModal');
+const btnClose   = document.querySelector('#btnClose');
 
+/*Eventos */
 bntRules.addEventListener('click', ()=>{
     rulesModal.classList.remove('hidden');
+});
+
+btnClose.addEventListener('click', ()=> {
+    rulesModal.classList.add('hidden');
 });
