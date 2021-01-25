35603ae14dbaaed17f1b664ce2f702e6b1b4742d
ferras <osocaramelosofer@hotmail.com>
2021-01-24 20:09:29 -0600

    empezando logica js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ const bntRules   = document.querySelector('.rulesModalButton');
 const rulesModal = document.querySelector('.rulesModal');
 const btnClose   = document.querySelector('#btnClose');
 
+const btns       = document.querySelectorAll('.btn').className;
+const classeBtns = ["hand-paper","hand-rock"];
+console.log(btns);
+
 /*Eventos */
 bntRules.addEventListener('click', ()=>{
     rulesModal.classList.remove('hidden');
