aefe7b1a9b751c9016ed834761e9f13f1e404dac
Alonso Hernández Sastré <57261289+alonsohs@users.noreply.github.com>
2021-01-26 15:56:32 -0600

    Lógica iniciada

--- js/app.js
+++ js/app.js
@@ -1,16 +1,39 @@
-const bntRules   = document.querySelector('.rulesModalButton');
-const rulesModal = document.querySelector('.rulesModal');
-const btnClose   = document.querySelector('#btnClose');
+import { compareBoard } from "./templates/gameContainer";
+const gameContainer = document.querySelector(".gameContainer");
 
-const btns       = document.querySelectorAll('.btn').className;
-const classeBtns = ["hand-paper","hand-rock"];
-console.log(btns);
+const bntRules = document.querySelector(".rulesModalButton");
+const rulesModal = document.querySelector(".rulesModal");
+const btnClose = document.querySelector("#btnClose");
+
+const btnPaper = document.querySelector("#btnPaper");
+const btnScissors = document.querySelector("#btnScissors");
+const btnRock = document.querySelector("#btnRock");
+
+const options = [btnPaper, btnScissors, btnRock];
 
 /*Eventos */
-bntRules.addEventListener('click', ()=>{
-    rulesModal.classList.remove('hidden');
+bntRules.addEventListener("click", () => {
+  rulesModal.classList.remove("hidden");
 });
 
-btnClose.addEventListener('click', ()=> {
-    rulesModal.classList.add('hidden');
+btnClose.addEventListener("click", () => {
+  rulesModal.classList.add("hidden");
 });
+
+options.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    compare(btn.value);
+  });
+});
+
+const compare = (userChoise) => {
+  //compareView();
+  console.log("click");
+  const random = Math.floor(Math.random() * (4 - 1) + 1);
+};
+
+const compareView = () => {
+  console.log(gameContainer);
+
+  gameContainer.innerHTML = compareBoard;
+};
---
+++ js/templates/compareBoard.js
@@ -0,0 +1,21 @@
+export const compareBoard = `
+<div class="compareBoard">
+  <div class="optionPicked">
+      <h2 class="title">you picked</h2>
+      <div class="option">
+          <div class="option__border">
+              <div class="option__content">
+                  <img src="./images/icon-rock.svg" alt="option">
+              </div>
+          </div>
+      </div>
+  </div>
+  <div class="optionPicked">
+      <h2 class="title">you picked</h2>
+      <div class="optionHouse">
+          <div class="optionHouse__circle">
+          </div>
+      </div>
+  </div>
+</div>
+`
