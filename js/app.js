2aaf1d57ff7bf3ce4e894cbe86ebcbc27d723548
ferras <osocaramelosofer@hotmail.com>
2021-01-27 11:04:45 -0600

    lógicaj terminada

--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
 
+
 const gameApp = document.querySelector(".gameApp");
 
 const bntRules = document.querySelector(".rulesModalButton");
 const rulesModal = document.querySelector(".rulesModal");
 const btnClose = document.querySelector("#btnClose");
 
-const btnPaper = document.querySelector("#btnPaper");
-const btnScissors = document.querySelector("#btnScissors");
-const btnRock = document.querySelector("#btnRock");
+const score = document.querySelector('.modal__score--number');
 
-const options = [btnPaper, btnScissors, btnRock];
-
-/*Eventos */
 bntRules.addEventListener("click", () => {
   rulesModal.classList.remove("hidden");
 });
@@ -20,16 +16,50 @@ btnClose.addEventListener("click", () => {
   rulesModal.classList.add("hidden");
 });
 
-options.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    compare(btn.value);
-  });
-});
 
-const compare = (userChoise) => {
+
+
+const initGame = () => {
+
+  score.innerText = window.localStorage.getItem('score') || '0';
+  gameApp.innerHTML = initBoard;
+
+  const btnPaper = document.querySelector("#btnPaper");
+  const btnScissors = document.querySelector("#btnScissors");
+  const btnRock = document.querySelector("#btnRock");
+  const options = [btnPaper, btnScissors, btnRock];
+
+  options.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      compare(btn.value, options);
+    });
+  });
+  
+
+
+
+
+}
+
+initGame();
+
+
+
+const compare = (userChoise, options) => {
+  options.forEach((btn) => {
+    btn.removeEventListener('click',()=>{});
+  });  
+
   compareView(userChoise);
-  console.log("click");
   const random = Math.floor(Math.random() * (4 - 1) + 1);
+  setTimeout(() => {
+    const optionHouse = document.querySelector('.optionHouse');
+    optionHouse.innerHTML = houseOption(random);
+
+    showResult(userChoise, random);
+  }, 1500);
+
+
 };
 
 const compareView = (userChoise) => {
@@ -37,4 +67,28 @@ const compareView = (userChoise) => {
 
   gameApp.innerHTML = compareBoard(userChoise);
 };
+
+const showResult = (userChoise, houseChoise) => {
+  let result = null;
+  const resultContainer = document.querySelector('.resultContainer');
+  const btnPlayAgain = document.querySelector('.btn-playAgain');
+  if (userChoise == houseChoise ) {
+    result = 0;
+  } else if  ((userChoise == 1 && houseChoise ==3) || (userChoise == 3 && houseChoise ==2) || (userChoise == 2 && houseChoise ==1)) {
+    result = 1;
+  } else {
+    result = -1;
+  }
+
+
+  resultContainer.innerHTML = resultMessage(result);
+
+  if ( result == 1 ) {
+    const userScore = window.localStorage.getItem('score') || '0';
+    const newSCore = parseInt(userScore) + 1;
+    window.localStorage.setItem('score', String(newSCore));
+  }
+};
+
+
 
---
+++ js/templates/templates.js
@@ -0,0 +1,73 @@
+const compareBoard = (option) => `
+<div class="compareBoard">
+  <div class="optionPicked">
+      <h2 class="title">you picked</h2>
+      <div class="option compared">
+          <div class="option__border" style="background-image: linear-gradient(${
+              option == 1 ?  ' hsl(230, 89%, 62%) , hsl(230, 89%, 65%)' :
+              option == 2 ? 'hsl(39, 89%, 49%) , hsl(40, 84%, 53%)' :
+              ' hsl(349, 71%, 52%) , hsl(349, 70%, 56%)'
+          });">
+              <div class="option__content">
+                  <img src="./assets/icon-${option == 1 ? 'paper' : option == 2 ? 'scissors' : 'rock'}.svg" alt="option">
+              </div>
+          </div>
+      </div>
+  </div>
+
+
+  <div class="resultContainer"> </div>
+
+  <div class="optionPicked">
+      <h2 class="title">The house picked</h2>
+      <div class="optionHouse">
+      </div>
+  </div>
+</div>
+`
+const houseOption = (option) => `
+<div class="option compared">
+    <div class="option__border" style="background-image: linear-gradient(${
+        option == 1 ?  ' hsl(230, 89%, 62%) , hsl(230, 89%, 65%)' :
+        option == 2 ? 'hsl(39, 89%, 49%) , hsl(40, 84%, 53%)' :
+        ' hsl(349, 71%, 52%) , hsl(349, 70%, 56%)'
+    });">
+        <div class="option__content">
+            <img src="./assets/icon-${option == 1 ? 'paper' : option == 2 ? 'scissors' : 'rock'}.svg" alt="option">
+        </div>
+    </div>
+</div>
+`
+
+const resultMessage = (result) => `
+<div class="resultMessage">
+    <h1 class="result">${result == -1 ? 'you lose' : result == 1 ? 'you win' : 'drow'}</h1>
+    <button class="btn-playAgain" onClick="initGame()">play again</button>
+</div>
+`
+
+const initBoard = `
+<div class="gameBoard">
+<button id="btnRock" class="option option1" value="1">
+  <div class="option__border1">
+    <div class="option__content">
+      <img src="./assets/icon-paper.svg" alt="paper" />
+    </div>
+  </div>
+</button>
+<button id="btnScissors" class="option option2" value="2">
+  <div class="option__border2">
+    <div class="option__content">
+      <img src="./assets/icon-scissors.svg" alt="scissors" />
+    </div>
+  </div>
+</button>
+<button id="btnPaper" class="option option3" value="3">
+  <div class="option__border3">
+    <div class="option__content">
+      <img src="./assets/icon-rock.svg" alt="rock" />
+    </div>
+  </div>
+</button>
+</div>
+`
